d47d50f50f0113dedc5f9d93cc7089ccbc401f88
tndud <tndud4327@naver.com>
2024-07-05 13:03:47 +0900

    update class

---
+++ adv_basics/lib/start_screen.dart
@@ -0,0 +1,53 @@
+//사용자 지정 시작 스크린
+
+//화면이 변하는 것이 아니니까 StatefulWidget이 아닌 StateelssWidget을 사용하는 것이 맞다.
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // Opacity( //투명도 설정을 하는 것 보다 ARGB에서 투명도를 낮추는 것을 선호한다. 
+          //   opacity: 0.6,
+          //   child: Image.asset(
+          //     'assets/images/quiz-logo.png',
+          //     width: 300,
+          //   ),
+          // ),
+          Image.asset(
+            'assets/images/quiz-logo.png',
+             width: 300,
+             color: const Color.fromARGB(150, 255, 255, 255)
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn Fultter the fun way!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+              //child가 아닌 labe을 사용해야함..
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+
+                  
+              ),
+              icon:
+                  const Icon(Icons.arrow_right_alt), //이미 저장되어있는 아이콘 사용해서아이콘 만들기
+              label: const Text('Start Quiz'))
+        ],
+      ),
+    );
+  }
+}
