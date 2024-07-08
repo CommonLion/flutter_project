30ee2f2c7f328799e4ca517ba11bc81fbb99cee2
tndud <tndud4327@naver.com>
2024-07-08 10:12:42 +0900

    update class
    create the question button

---
+++ adv_basics/lib/models/quiz_questions.dart
@@ -0,0 +1,7 @@
+//데이터 저장을 위한 클래스
+class QuizQuestions {
+  QuizQuestions(this.text, this.answers); // 인수를 받을 생성자임.
+
+  final String text;
+  final List<String> answers;
+}
--- adv_basics/lib/quiz.dart
+++ adv_basics/lib/quiz.dart
@@ -12,23 +12,23 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
+  
+  //Widget? activeScreen;  
   //var이 아닌 widget으로 해야함.
-  //여기 Quiz state에서는 스위치 스크린에서 StartScreen에 겂으로 포인터를 전달하고 있다.
+  //여기 Quiz state에서는 스위치 스크린에서 StartScreen에 겂으로 포인터를 전달하고 있다. 
   //이제는 acticeScreen을 문자열로 해서 렌더링될때 실제 위젯으로 추론하게 만든다는거임
-  //var activeScreen = 'start-screen';
+  var activeScreen = 'start-screen';
 
   @override
-  void initState() {
-    //상태를 초기화하는 구문. 한번실행되고 다시는 실행 안됨.
-    activeScreen = StartScreen(switchScreen);
-    //activeScreen = 'questions-screen';
+  void initState() { //상태를 초기화하는 구문. 한번실행되고 다시는 실행 안됨.
+    //activeScreen = StartScreen(switchScreen);
+    activeScreen = 'question-screen';
     super.initState();
   }
 
   void switchScreen() {
     setState(() {
-      activeScreen = QuestionsScreen();
+      activeScreen = 'question-screen';
     });
   }
 
@@ -47,7 +47,9 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen,
+          child: activeScreen == 'start-screen'
+            ? StartScreen(switchScreen)
+            : const QuestionsScreen(),
         ),
       ),
     );
