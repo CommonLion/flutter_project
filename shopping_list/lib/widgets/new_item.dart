dcadf92a3004658094fdd48c534c591a5171110a
tndud <tndud4327@naver.com>
2024-07-11 17:31:27 +0900

    update class
    modify new_item widget

--- shopping_list/lib/widgets/new_item.dart
+++ shopping_list/lib/widgets/new_item.dart
@@ -1,6 +1,8 @@
 //사용자의 입력 관리.
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -17,7 +19,63 @@ class _NewItemState extends State<NewItem> {
       appBar: AppBar(
         title: const Text('Add a new item'),
       ),
-      body: const Padding(padding: EdgeInsets.all(12), child: Text('The form')),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text('Name'),
+                ),
+                validator: (value) {
+                  return 'Demo...';
+                },
+              ), //instead of TextField()
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    //텍스트 양식 필드를 확장된 위젯으로 래핑한다.
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  //카테고리 선택하는 버튼/ 또한 버튼 약식 필드 위젯을 중심으로 확장한다.
+                  Expanded(
+                    child: DropdownButton(items: [
+                      for (final category in categories.entries)
+                        DropdownMenuItem(
+                          value: category.value,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 16,
+                                height: 16,
+                                color: category.value.color,
+                              ),
+                              const SizedBox(
+                                width: 6,
+                              ),
+                              Text(category.value.title),
+                            ],
+                          ),
+                        ),
+                    ], onChanged: (value) {}),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ), //Form이 화면 유효성 검사도 해준다.
     );
   }
 }
